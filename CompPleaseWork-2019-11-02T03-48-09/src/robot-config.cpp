51fd1257c7473837021ca180cc4b72503c963e3c
MSMS-Robotics <57278838+MSMS-Robotics@users.noreply.github.com>
2019-11-01 22:50:13 -0500

    OGTankDrive
    All the original CompPleaseWork code. Basic drivetrain controls (tank drive).

---
+++ CompPleaseWork-2019-11-02T03-48-09/src/robot-config.cpp
@@ -0,0 +1,16 @@
+#include "vex.h"
+
+using namespace vex;
+
+// A global instance of brain used for printing to the V5 brain screen
+brain Brain;
+motor Right = motor(1, true);
+motor Left = motor(2);
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ *
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit(void) {
+  // Nothing to initialize
+}
